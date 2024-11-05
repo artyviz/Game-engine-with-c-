fb2e0149f9510bea5af937d7f578e10c2a0e858c
farhan <141487224+artyviz@users.noreply.github.com>
2024-11-05 15:04:08 +0530

    editor for game engine

---
+++ editor/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using editor.gameproject;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+
+namespace editor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OnToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+    }
+}
---
+++ editor/gameproject/ProjectBrowserDialog.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace editor.gameproject
+{
+    public partial class ProjectBrowserDialog : Window
+    {
+        private MainWindow _mainWindow;
+
+        public ProjectBrowserDialog(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+        }
+        private void OnToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == _mainWindow.openProjectButton)
+            {
+                if (_mainWindow.createProjectButton.IsChecked == true)
+                {
+                    _mainWindow.createProjectButton.IsChecked = false;
+                    _mainWindow.browserContent.Margin = new Thickness(0);
+                }
+                _mainWindow.openProjectButton.IsChecked = true;
+            }
+            else if (sender == _mainWindow.createProjectButton)
+            {
+                if (_mainWindow.openProjectButton.IsChecked == true)
+                {
+                    _mainWindow.openProjectButton.IsChecked = false;
+                    _mainWindow.browserContent.Margin = new Thickness(-800, 0, 0, 0);
+                }
+                _mainWindow.createProjectButton.IsChecked = true;
+            }
+        }
+    }
+}
